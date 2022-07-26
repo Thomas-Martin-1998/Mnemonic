4833584aceae8cc78fc06aa9495bb0f019fe2e55
Thomas Martin <Invictus.Martin@gmail.com>
2022-07-26 20:30:43 +0100

    Initial Commit

---
+++ Mnemonic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mnemonic
+//
+//  Created by Thomas Martin on 26/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
